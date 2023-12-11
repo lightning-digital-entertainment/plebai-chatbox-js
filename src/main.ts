27b0d9ffe3a4c570ed1fc5cd082bfc1540399592
Egge <egge24@protonmail.com>
2023-12-11 18:03:52 +0100

    added helpbutton + refactor

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { createCloseButton, createHelpButton } from "./elements";
 import styles from "./plebai.module.css";
 import { Conversation } from "plebai-sdk";
 
@@ -67,15 +68,17 @@ function setUp() {
   topBarTitle.innerText = plebAiConf.chatTitle;
   topBar.appendChild(topBarTitle);
 
-  const closeButton = document.createElement("button");
-  closeButton.className = styles["plebai-chat-close"];
-  closeButton.innerHTML =
-    '<svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>';
-  closeButton.onclick = () => {
+  const buttonContainer = document.createElement("div");
+  buttonContainer.className = styles["plebai-chat-button-container"];
+
+  const helpButton = createHelpButton(escalateIssue);
+  const closeButton = createCloseButton(() => {
     chatContainer.style.visibility = "hidden";
     chatButton.style.visibility = "visible";
-  };
-  topBar.appendChild(closeButton);
+  });
+  buttonContainer.appendChild(helpButton);
+  buttonContainer.appendChild(closeButton);
+  topBar.appendChild(buttonContainer);
 
   const chatInner = document.createElement("div");
   chatInner.className = styles["plebai-chat-inner"];
